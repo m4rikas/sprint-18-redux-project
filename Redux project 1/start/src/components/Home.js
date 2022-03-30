753a3f602e78a43f2355ff5dd12474a1edcce85c
Marius Krenevičius <86297482+m4rikas@users.noreply.github.com>
2022-03-30 14:08:22 +0300

    Update Home.js

--- Redux project 1/start/src/components/Home.js
+++ Redux project 1/start/src/components/Home.js
@@ -1,11 +1,1 @@
-import React from "react";
-
-function HomePage() {
-  return (
-    <div className="col-md-6 col-md-offset-3">
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-export default HomePage;
+/* Write your code here */
