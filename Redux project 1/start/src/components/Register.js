b650928e29d8a292cb1cfec3d14c1da9f4d208da
Marius Krenevičius <86297482+m4rikas@users.noreply.github.com>
2022-03-30 13:55:00 +0300

    Update Register.js

--- Redux project 1/start/src/components/Register.js
+++ Redux project 1/start/src/components/Register.js
@@ -1,9 +1,1 @@
-const Register = () => {
-  return (
-    <div className="col-md-6 col-md-offset-3">
-      <h2>Register</h2>
-    </div>
-  );
-};
-
-export default Register;
+/* Write your code here */ 
